233c6949fe85249b54abd0921956329d894f8b6b
Partha <partha4224@gmail.com>
2020-02-27 00:09:55 +0530

    Update Login & register

--- vidly-front/src/components/registerForm.jsx
+++ vidly-front/src/components/registerForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Joi from "joi-browser";
 import Form from "./common/form";
+import * as userService from '../services/userService';
 
 class RegisterForm extends Form {
   state = {
@@ -22,9 +23,19 @@ class RegisterForm extends Form {
       .label("Name")
   };
 
-  doSubmit = () => {
-    // Call the server
-    console.log("Submitted");
+  doSubmit = async () => {
+    try {
+      const response = await userService.register(this.state.data);
+      localStorage.setItem('token', response.headers['x-auth-token']);
+      this.props.history.push('/');
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.username = ex.response.data;
+        this.setState({ errors });
+      }
+
+    }
   };
 
   render() {

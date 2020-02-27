67916bbcccb24725074977a481dfaf43ef380249
Partha <partha4224@gmail.com>
2020-02-27 22:55:54 +0530

    Authentica for Add or Update Movie

--- vidly-front/src/services/httpService.js
+++ vidly-front/src/services/httpService.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import logger from './logService';
 import { toast } from 'react-toastify';
 
+
 axios.interceptors.response.use(null, error => {
     const expectedError = error.response && error.response.status >= 400 && error.response.status < 500;
 
@@ -15,9 +16,15 @@ axios.interceptors.response.use(null, error => {
 });
 
 
+function setJwt(jwt) {
+    axios.defaults.headers.common['x-auth-token'] = jwt;
+}
+
+
 export default {
     get: axios.get,
     post: axios.post,
     put: axios.put,
-    delete: axios.delete
+    delete: axios.delete,
+    setJwt
 };
